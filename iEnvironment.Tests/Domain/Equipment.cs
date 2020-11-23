736b554eec34518e42d0334d8440bc73d5833a89
Mateus Moura Santos <msantos.96@outlook.com>
2020-11-23 10:01:34 -0300

    Test Project configured

---
+++ iEnvironment.Tests/Domain/Equipment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using iEnvironment.Domain.Models;
+using Xunit;
+
+namespace iEnvironment.Tests.Domain
+{
+    public class EquipmentTests
+    {
+        [Fact]
+        public void CriarEquipamento()
+        {
+            //Arrange
+            var equipamento = new Equipment{ Enabled = true };
+
+            //Act
+            equipamento.UpdateValue("teste", true);
+
+            //Assert
+            Assert.Equal("teste", equipamento.CurrentValue);
+        }
+
+
+    }
+}
