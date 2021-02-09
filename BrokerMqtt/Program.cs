21bead86281997759d88033a542483e686a9ef8f
mateus moura santos <msantos.96@outlook.com>
2021-02-09 16:40:21 -0300

    Broker project added, packages configured

---
+++ BrokerMqtt/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+
+namespace BrokerMqtt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json", false, true);
+
+            var config = builder.Build();
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
