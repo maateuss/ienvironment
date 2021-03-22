e594568ebd8fdce5695ef5133d80427d85143ced
mateus moura santos <msantos.96@outlook.com>
2021-03-22 19:39:06 -0300

    Image controller created

---
+++ iEnvironment.RestAPI/Services/ImageService.cs
@@ -0,0 +1,61 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using iEnvironment.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iEnvironment.RestAPI.Services
+{
+    public class ImageService : BaseService<Image>
+    {
+        private AmazonS3Client AwsClient;
+        public ImageService() : base("images")
+        {
+            AwsClient = new AmazonS3Client(Settings.AccessKey, Settings.AccessSecret, Amazon.RegionEndpoint.SAEast1);
+        }
+
+
+        public async Task<Image> UploadPhoto(IFormFile file)
+        {
+
+            byte[] fileBytes = new byte[file.Length];
+            file.OpenReadStream().Read(fileBytes, 0, int.Parse(file.Length.ToString()));
+            var image = new Image();
+
+            var fileName = DateTime.Now.ToLongTimeString().Replace(".", "").Replace(":", "").Replace(" ", "") + file.FileName;
+            image.FileName = fileName;
+            image.Size = Convert.ToInt32(file.Length);
+
+
+            PutObjectResponse response = null;
+
+            using (var stream = new MemoryStream(fileBytes))
+            {
+                var request = new PutObjectRequest
+                {
+                    BucketName = Settings.Bucket,
+                    Key = fileName,
+                    InputStream = stream,
+                    ContentType = file.ContentType,
+                    CannedACL = S3CannedACL.PublicRead
+                };
+
+                response = await AwsClient.PutObjectAsync(request);
+            };
+
+            if (response.HttpStatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception("Erro na imagem");
+            }
+
+
+
+            return image;
+        }
+
+    }
+}
--- iEnvironment.RestAPI/Settings.cs
+++ iEnvironment.RestAPI/Settings.cs
@@ -11,15 +11,23 @@ namespace iEnvironment.RestAPI
         public static string MongoConnectionString { get => mongoConnString; }
         public static string Database { get => database; }
         public static int WorkFactor { get => workFactor; }
+        public static string AccessKey { get => accessKey; }
+        public static string AccessSecret { get => accessSecret; }
+        public static string Bucket { get => bucket; }
         private static string mongoConnString { get; set; }
         private static string database { get; set; }
         private static int workFactor { get; set; }
-
+        private static string accessKey { get; set; }
+        private static string accessSecret { get; set; }
+        private static string bucket { get; set; }
         public static void Configure(IConfiguration configuration)
         {
             mongoConnString = configuration["MongoCS"];
             database = configuration["Database"];
             workFactor = int.Parse(configuration["WorkFactor"]);
+            accessKey = configuration["AccessKey"];
+            accessSecret = configuration["AccessSecret"];
+            bucket = configuration["bucket"];
         }
     }
 }
